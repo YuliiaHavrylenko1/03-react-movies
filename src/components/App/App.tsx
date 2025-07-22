8166b1da8123250066efdfb76a86e430dfdcc41b
YuliiaHavrylenko1 <gavrilenko.j94@gmail.com>
2025-07-22 22:43:42 +0100

    changed

---
+++ src/components/App/App.tsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+
+import SearchBar from '../SearchBar/SearchBar';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import MovieModal from '../MovieModal/MovieModal';
+
+import { fetchMovies } from '../../services/movieService';
+import type { Movie } from '../../types/movie';
+
+export default function App() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [query, setQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    if (!query) return;
+
+    setLoading(true);
+    setError('');
+    setMovies([]);
+
+    fetchMovies(query)
+      .then(results => {
+        if (results.length === 0) {
+          toast('No movies found for your request.');
+        }
+        setMovies(results);
+      })
+      .catch(() => {
+        setError('There was an error, please try again...');
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [query]);
+
+  const handleSearch = (searchQuery: string) => {
+    setQuery(searchQuery);
+  };
+
+  const handleSelectMovie = (movie: Movie) => {
+    setSelectedMovie(movie);
+    document.body.style.overflow = 'hidden'; // блокування скролу при відкритті модалки
+  };
+
+  const handleCloseModal = () => {
+    setSelectedMovie(null);
+    document.body.style.overflow = ''; // розблокування скролу
+  };
+
+  return (
+    <>
+      <SearchBar onSubmit={handleSearch} />
+      <Toaster />
+      {loading && <Loader />}
+      {error && <ErrorMessage />}
+      {!loading && !error && <MovieGrid movies={movies} onSelect={handleSelectMovie} />}
+      {selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={handleCloseModal} />
+      )}
+    </>
+  );
+}
